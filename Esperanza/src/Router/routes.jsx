2d4bea6f8ebd986f3308a8cd2536c9e89e8ffdf5
Janis Aravena <janisaravenaespinoza@gmail.com>
2024-04-20 16:10:22 -0400

    se quita home, inicia de inmediato en /

--- Esperanza/src/Router/routes.jsx
+++ Esperanza/src/Router/routes.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 
-import Inicio from "../Views/Inicio";
 import LoginRegistro from "../Views/LoginRegistro";
 import Registros from "../Views/Registros";
 import Header from "../Views/SesionIniciada"
 
 
 const router = createBrowserRouter([
-  { path: "/", element: <Inicio /> },
-  { path: "/LoginRegistro", element: <LoginRegistro/> },
+  { path: "/", element: <LoginRegistro/> },
   { path: "/Login", element: <Registros /> },
   { path: "/Header", element: <Header/> },
  
