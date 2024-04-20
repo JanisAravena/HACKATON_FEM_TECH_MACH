42441b3262b71097342efdb98124c3417ebf08e0
CarolinaReyes <karo.rg2312@gmail.com>
2024-04-19 20:31:06 -0400

    Formulario de registro

--- Esperanza/src/Router/routes.jsx
+++ Esperanza/src/Router/routes.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 
 import Inicio from "../Views/Inicio";
+import Registros from "../Views/Registros";
 
 
 const router = createBrowserRouter([
   { path: "/", element: <Inicio /> },
+  { path: "/Login", element: <Registros /> },
  
 ]);
 
